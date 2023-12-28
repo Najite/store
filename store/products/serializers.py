b263b5cdfa8a381ec171ae491f3ab58a1edd20dc
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2023-12-28 11:56:15 +0100

    made changes to ensure the name of the user is displayed and not id

--- store/products/serializers.py
+++ store/products/serializers.py
@@ -1,12 +1,22 @@
-import rest_framework
 from rest_framework import serializers
 from .models import Product
 
 
+
 class ProductListSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(read_only=True)
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ['id',
+                  'user',
+                  'name',
+                  'price',
+                  'image',
+                  'created_at',
+                  'updated_at',
+                  'category'
+                  
+                  ]
 
 
 
